236b435bb819b06ad7d3c275b3e6adffbb26612b
hkwithhk <hkwithhk@gmail.com>
2024-08-26 18:39:10 +0900

    vfd

--- app/src/main/java/com/example/dietmanagement/LoginActivity.java
+++ app/src/main/java/com/example/dietmanagement/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.dietmanagement;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button signUpBTN_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,15 @@ public class LoginActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        signUpBTN_login = findViewById(R.id.signUpBTN_login);
+
+        signUpBTN_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),SignUpActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
