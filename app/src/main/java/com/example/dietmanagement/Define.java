bc84fa88473f95b60e2fe3f10654a261057714a7
hkwithhk <hkwithhk@gmail.com>
2024-09-02 18:31:44 +0900

    Define추가

---
+++ app/src/main/java/com/example/dietmanagement/Define.java
@@ -0,0 +1,12 @@
+package com.example.dietmanagement;
+
+public class Define {
+
+    public String userId = "";
+    public String nickName = "";
+    public String pw = "";
+    public int height = 0;
+    public int weight = 0;
+    public String gender = "";
+
+}
